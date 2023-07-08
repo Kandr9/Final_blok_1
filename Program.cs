c21485fd306a317f3cfc8bc6581e3899fa575b69
Kandr9 <kandr13579@gmail.com>
2023-07-08 20:07:05 +0300

    Проверка массива

--- Program.cs
+++ Program.cs
@@ -3,3 +3,13 @@
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
 
 string [] startingArray = new string[] {"main", "333", "world", "999"};
+string[] resultArray = new string [0];
+
+foreach (var item in startingArray)
+{
+    if (item.Length <= 3)
+    {
+    Array.Resize(ref resultArray, resultArray.Length + 1);
+    resultArray [resultArray.Length-1] = item;
+    }
+}
